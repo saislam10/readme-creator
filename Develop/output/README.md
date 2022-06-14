e824f88c2156c308b5a21582be7b0dd4c254179d
Safwan Islam <saislam@davidson.edu>
2022-06-14 13:48:47 -0500

    set up table of contents with the rest of the ReadMe requirements

---
+++ Develop/output/README.md
@@ -0,0 +1,38 @@
+
+# ReadMe
+
+# Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contribution](#contribution)
+5. [Tests](#tests)
+6. [License](#license)
+7. [Questions](#questions)
+
+## Description
+lalala
+
+## Installation
+lalal
+
+## Usage
+lalla
+
+## Contribution
+lalal
+
+## Tests
+llaaala
+
+## License
+<img src="https://img.shields.io/badge/license-Apache-blue" alt="badge-Apache" />
+
+## Questions
+If you have any questions, please visit the following:
+
+GitHub username: saislam10
+
+Email: saislam10@gmail.com
+
+    
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,22 +1,65 @@
 const fs = require('fs');
+const { ReadMe } = require('lodash');
 
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const generateMarkdown = ({
+  repository,
+  description,
+  installation,
+  usage,
+  contribution,
+  tests,
+  license,
+  gitHub,
+  email
+})=> {
+  console.log('GENERATING PAGE…');
+  const ReadMe = (
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+    `
+# ${repository}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+# Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Usage](#usage)
+4. [Contribution](#contribution)
+5. [Tests](#tests)
+6. [License](#license)
+7. [Questions](#questions)
 
-`;
-}
+## Description
+${description}
 
-module.exports = generateMarkdown;
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contribution
+${contribution}
+
+## Tests
+${tests}
+
+## License
+<img src="https://img.shields.io/badge/license-${license}-blue" alt="badge-${license}" />
+
+## Questions
+If you have any questions, please visit the following:
+
+GitHub username: ${gitHub}
+
+Email: ${email}
+
+    `
+  );
+  fs.writeFileSync('./output/README.md', ReadMe);
+  console.log('ReadMe GENERATED!');
+  process.exit();
+};
+
+module.exports = {
+  generateMarkdown
+};
