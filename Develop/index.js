73627e62353bfec290b66300cbb1672768ae5884
Safwan Islam <saislam@davidson.edu>
2022-06-14 12:54:38 -0500

    followed instructor code and set up code in order to generate read me

--- Develop/index.js
+++ Develop/index.js
@@ -1,49 +1,114 @@
 // TODO: Include packages needed for this application
-const { prompt } = require('inquirer');
-const fs = require('fs');
+const inquirer = require('inquirer');
+const utils = require("./utils/generateMarkdown");
+const questions = [
+    {
+        type: 'input',
+        name: 'repository',
+        message: 'What is your the name of your repository?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is a description of your product?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install the product?:',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your product?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can others contribute?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'How do you test your product?',
+    },
+    {
+        type: 'rawlist',
+        name: 'license',
+        message: 'What License was this created with?',
+        choices: [
+            "MPL",
+            "GPL",
+            "Apache",
+            "MIT",
+            "CC",
+            "BSD",
+        ]
+    },
+    {
+        type: 'input',
+        name: 'gitHub',
+        message: 'What is your GitHub Username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+
+
+
+
+]
+
+inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+    utils.generateMarkdown(answers);
+}
+)
 // TODO: Create an array of questions for user input
-const init = async () => {
-    const data = await prompt([
-        {
-            type: 'input',
-            name: 'repository',
-            message: 'Description:',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Description:',
-        },
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Installation:',
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Usage:',
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Contributions:',
-        },
-        {
-            type: 'input',
-            name: 'tests',
-            message: 'Tests:',
-        },
-    ])
-// building readme document, using template literals
-// TODO: Create a function to write README file
-const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+// const init = async () => {
+//     const data = await prompt([
+//         {
+//             type: 'input',
+//             name: 'repository',
+//             message: 'What is your the name of your repository?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'description',
+//             message: 'What is a description of your product?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'installation',
+//             message: 'How do you install the product?:',
+//         },
+//         {
+//             type: 'input',
+//             name: 'usage',
+//             message: 'How do you use your product?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'contribution',
+//             message: 'How can others contribute?',
+//         },
+//         {
+//             type: 'input',
+//             name: 'tests',
+//             message: 'How do you test your product?',
+//         },
+//     ])
+// // building readme document, using template literals
+// // TODO: Create a function to write README file
+// const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
-    err ? console.log(err) : console.log('Success!')
-    );
-};
-// TODO: Create a function to initialize app
+// fs.writeFile(filename, JSON.stringify(data, null, 2), (err) =>
+//     err ? console.log(err) : console.log('Success!')
+//     );
+// };
+// // TODO: Create a function to initialize app
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
